f74a9a840f9b2fa5f53f5497231d4b451f04b8f8
Aaron Liao <aliao4@uci.edu>
2019-01-03 21:12:58 -0800

    added exact word search regex

--- content.js
+++ content.js
@@ -1,16 +1,15 @@
 var elements = document.getElementsByTagName('*');
-
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
 
     for (var j = 0; j < element.childNodes.length; j++) {
         var node = element.childNodes[j];
-        
+
         if (node.nodeType === Node.TEXT_NODE) {
             var text = node.nodeValue;
             var replacedText = text;
             for (var candidate in dict){
-                var re = new RegExp(candidate, "gi");
+                var re = new RegExp('^'+candidate+'$', "gi");
                 replacedText = replacedText.replace(re, dict[candidate]);
             }
             if (replacedText !== text) {
--- dictionary.js
+++ dictionary.js
@@ -1,5 +1,8 @@
 var dict = {
 	"banana": "香蕉",
 	"pineapple": "菠萝",
-	"apple": "苹果"
+	"apple": "苹果",
+	"book": "书",
+	"child": "小孩",
+	"ocean": "海"
 }
