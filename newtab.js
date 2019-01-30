0e9e00fa9967b655c3a2bae5864a4dd2627fe99d
MikeBikeLA <michael.mu.sun@gmail.com>
2019-01-29 18:53:32 -0800

    add language dropdown menu

--- newtab.js
+++ newtab.js
@@ -5,10 +5,45 @@ function load_new_tab(){
     
         console.log("NUMBER OF WORDS LEARNED");
         console.log(num_words_learned);
-
      });
-
-
 }
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dropdownFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+  
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+//JS for drop down on click
+document.addEventListener('DOMContentLoaded', function() {
+    var link = document.getElementById('dropdownID');
+    link.addEventListener('click', function() {
+        dropdownFunction();
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var languageSelection = document.getElementgetElementByClassName('dropdown-language');
+    languageSelection.addEventListener('click', function() {
+        var selectedLanguage = languageSelection.innerHTML;
+        var selectedLanguage = '4';
+        document.getElementById("myDropdown").innerHTML = selectedLanguage;
+    });
+});
+
 load_new_tab();
 console.log("newtab.js loaded");
