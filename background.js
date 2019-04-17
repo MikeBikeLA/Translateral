9ad4370140120997a51c466d64a9da5cf0cc8b28
Xenocidel <aaronliao714@gmail.com>
2019-04-16 23:38:16 -0700

    remove clearing of chrome storage upon install/startup

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 chrome.runtime.onInstalled.addListener(function() {
 	console.log("onInstalled called, loading dict");
-    chrome.storage.local.clear();
-    chrome.storage.sync.clear()
+    // chrome.storage.local.clear();
+    // chrome.storage.sync.clear()
     initialize_local_bank();
     
 });
 
 chrome.runtime.onStartup.addListener(function() {
 	console.log("onStartup called, loading dict");
-    chrome.storage.local.clear(); // todo: remove?
-    chrome.storage.sync.clear()
+    // chrome.storage.local.clear(); // todo: remove?
+    // chrome.storage.sync.clear()
     initialize_local_bank();
     
 });
