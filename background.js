653b563dc953a0385fec78f99a6e4e453786d1bf
Xenocidel <aaronliao714@gmail.com>
2019-04-27 15:22:02 -0700

    added blacklist backend functionality

--- background.js
+++ background.js
@@ -14,4 +14,27 @@ chrome.runtime.onStartup.addListener(function() {
     
 });
 
+function inBlacklist(url){
+    if (url.includes("chrome://") || url.includes("chrome-")){
+        return true; // skip chrome urls
+    }
+    chrome.storage.sync.get({"website_blacklist": {}}, function(website_blacklist_wrapper){
+        var website_blacklist = website_blacklist_wrapper.website_blacklist;
+        if (Object.keys(website_blacklist).length === 0){ return false; } // empty blacklist
+        for (const keyword of website_blacklist){
+            if (url.includes(keyword)) {
+                return true;
+            }
+        }
+        return false;
+    });
+}
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+    if (changeInfo.status === "complete" && !inBlacklist(tab.url)){
+        chrome.tabs.executeScript(null, {file: "content.js"});
+        console.log(tab.url);
+    }
+});
+
 console.log("background.js loaded");
